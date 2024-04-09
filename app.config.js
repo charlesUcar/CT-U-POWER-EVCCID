ff8be43544c998af59c8456f7e79481b1bd665fb
CharlesTseng <charlesucar@gmail.com>
2024-04-09 17:26:58 +0800

    chore: mutate conditional for project name and id
    add suffix "dev" in app id when env is development.
    for more conditional, use app.config.js file and remove app.json.

---
+++ app.config.js
@@ -0,0 +1,64 @@
+const IS_DEV = process.env.EXPO_PUBLIC_APP_ENV === 'development';
+
+export default {
+  name: IS_DEV ? 'U-POWER-EVCCID-Dev' : 'U-POWER-EVCCID',
+  slug: IS_DEV ? 'u-power-evccid-dev' : 'u-power-evccid',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#0096A3',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    infoPlist: {
+      NSCameraUsageDescription: 'Allow $(PRODUCT_NAME) to access your camera',
+    },
+    bundleIdentifier: IS_DEV ? 'com.upower.evccid.dev' : 'com.upower.evccid',
+    buildNumber: '1.0.0',
+    googleServicesFile: './GoogleService-Info.plist',
+  },
+  android: {
+    icon: './assets/icon.png',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#0096A3',
+    },
+    permissions: ['android.permission.CAMERA'],
+    package: IS_DEV ? 'com.upower.evccid.development' : 'com.upower.evccid',
+    googleServicesFile: './google-services.json',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {
+    eas: {
+      projectId: IS_DEV
+        ? '30a9ef20-70b4-49cc-a899-1868b0ab3eff'
+        : '7fa16fc6-0b91-4bd1-8695-a55464b0c2bb',
+    },
+  },
+  owner: 'charlesucar',
+  plugins: [
+    [
+      'react-native-vision-camera',
+      {
+        enableCodeScanner: true,
+      },
+    ],
+    '@react-native-firebase/app',
+    '@react-native-firebase/crashlytics',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          useFrameworks: 'static',
+        },
+      },
+    ],
+  ],
+};
--- app/services/Api/config.ts
+++ app/services/Api/config.ts
@@ -1,12 +1,14 @@
 // import Config from 'react-native-config';
 // import DeviceInfo from 'react-native-device-info';
 
+const ENV = process.env.EXPO_PUBLIC_APP_ENV;
+let DOMAIN = process.env.EXPO_PUBLIC_API_URL;
 
-const DOMAIN = process.env.EXPO_PUBLIC_API_URL;
 export const TIMEOUT = 30000;
 
 export const endpoints = {
   upower: `${DOMAIN}`,
+  env: `${ENV}`,
 };
 
 // export const AppVersion = DeviceInfo.getVersion();
