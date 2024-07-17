9af49355ba29b47d79897c2e3b8715abfe781d13
charlesx19 <charlesx19@gmail.com>
2024-07-17 12:27:58 +0800

    perf: improve flatList performance
    use react memo to avoid render to many times.

--- app/components/binding/BindingListCell/index.tsx
+++ app/components/binding/BindingListCell/index.tsx
@@ -1,7 +1,8 @@
-import { Text, View, Image } from "react-native";
-import Images from "../../../images";
-import styles from "./index.style";
-import React from "react";
+import { Text, View, Image } from 'react-native';
+import Images from '../../../images';
+import styles from './index.style';
+import React from 'react';
+import { memo } from 'react';
 
 type Vehicle = {
   vehicleId: string;
@@ -11,16 +12,18 @@ type Vehicle = {
   createdTime: string;
 };
 
-function BindingListCell({ item, index }: { item: Vehicle; index: number }) {
-  return (
-    <View style={styles.listHead}>
-      <Text style={styles.order}>{index}</Text>
-      <Text style={styles.vin}>{item.vin}</Text>
-      <View style={styles.evccId}>
-        {item.evccid ? <Image source={Images.Success_circle_fill} /> : null}
+const BindingListCell = memo(
+  ({ item, index }: { item: Vehicle; index: number }) => {
+    return (
+      <View style={styles.listHead}>
+        <Text style={styles.order}>{index}</Text>
+        <Text style={styles.vin}>{item.vin}</Text>
+        <View style={styles.evccId}>
+          {item.evccid ? <Image source={Images.Success_circle_fill} /> : null}
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
+);
 
 export default BindingListCell;
